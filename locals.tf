bc021e54a4addd5ba4a694660158000d87c39864
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 17:42:49 -0500

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  storage_account_names = [
+    "${var.first_name}mcit${var.last_name}1",
+    "${var.first_name}mcit${var.last_name}2",
+    "${var.first_name}mcit${var.last_name}3",  
+    "${var.first_name}mcit${var.last_name}4",
+    "${var.first_name}mcit${var.last_name}5",
+  ]
+}
