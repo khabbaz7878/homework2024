5a960e00235cad16c2eac0056e1be54d81c676f9
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 18:31:30 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,3 +6,18 @@ variable "first_name" {
 variable "last_name" {
   default = "islam" 
 }
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
