81fcd329b1340ad40f3a2e6f7f8019baed190aee
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 17:36:55 -0500

    Create resourcegroup.tf

---
+++ resourcegroup.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "mcit420zz5um" {
+  name     = "database-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = Homework2024.name
+  location                     = aHomework2024.location
+  version                      = "12.0"
+  administrator_login          = var.db_login
+  administrator_login_password = var.db_password
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
