2db44cf95f2dd313f013a7c19ebcf665f1eee4e3
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 18:50:49 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
 resource "azurerm_storage_account" "example" {
-2 count = length(var.storage_account_names)
-3 name                     = var.storage_account_names[count.index]
-4 resource_group_name      = azurerm_resource_group.example.name
-5 location                 = azurerm_resource_group.example.location
-6 account_tier             = "Standard"
-7 account_replication_type = "GRS"
-8}
+count = length(var.storage_account_names)
+name                     = var.storage_account_names[count.index]
+resource_group_name      = azurerm_resource_group.example.name
+location                 = azurerm_resource_group.example.location
+account_tier             = "Standard"
+account_replication_type = "GRS"
+}
