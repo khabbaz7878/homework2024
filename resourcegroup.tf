7561238ceea655c33019ad8089f29169a5a5c7c8
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 18:38:31 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,17 +1,5 @@
-resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "database-rg"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  name = "storage-rg"
+  location = "CanadaEast"
 }
-
-resource "azurerm_mssql_server" "example" {
-  name                         = "mssqlserver"
-  resource_group_name          = Homework2024.name
-  location                     = aHomework2024.location
-  version                      = "12.0"
-  administrator_login          = var.db_login
-  administrator_login_password = var.db_password
-  minimum_tls_version          = "1.2"
-}
-
-
 
