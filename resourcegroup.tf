0885abe650b75a37b6e746071bf5cfa210641bfe
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 18:59:38 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
-resource "azurerm_storage_account" "homeworkexam2024" {
+resource "azurerm_storage_account" "mcit420zz5um" {
 count = length(var.storage_account_names)
 name                     = var.storage_account_names[count.index]
-resource_group_name      = azurerm_resource_group.homeworkexam2024.name
-location                 = azurerm_resource_group.homeworkexam2024.location
+resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+location                 = azurerm_resource_group.mcit420zz5um.location
 account_tier             = "Standard"
 account_replication_type = "GRS"
 }
