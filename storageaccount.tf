332febff29a63b284f0bae33f1181c0df47776af
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-04 15:57:33 -0500

    Create storageaccount.tf

---
+++ storageaccount.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "storage" {
+  count = 5
+  name = "${lower(var.first_name)}${lower(var.last_name)}${count.index + 1}"
+  resource_group_name = homework20024-rg.rg.name
+  location = homework20024-rg.rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
