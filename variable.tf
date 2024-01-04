db59d98ee33b9e2960a44da065d1ef4473302845
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-04 15:08:33 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,32 +1,9 @@
-# Create list of storage account names
 variable "first_name" {
-  default = "sami"
+  default = "husni"
 }
 
 variable "last_name" {
-  default = "islam" 
-}
-variable "client_id" {
-  type = string
+  default = "khabbaz" 
 }
 
-variable "client_secret" {
-  type = string
-}
 
-variable "subscription_id" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-variable "storage_account_names" {
- default = [
-    "A.[firstnamemcitlastname1]",
-    "B.[firstnamemcitlastname2]",
-    "C.[firstnamemcitlastname3]",
-    "D.[firstnamemcitlastname4]",
-    "E.[firstnamemcitlastname5]"
- ]
-}
