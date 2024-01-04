15b72edc8960d6c32c36dcb0893cb6f6e4954a50
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-04 15:57:15 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -3,11 +3,3 @@ resource "azurerm_resource_group" "example" {
  location = "Canada East"
 }
 
-resource "azurerm_storage_account" "storage" {
-  count = 5
-  name = "${lower(var.first_name)}${lower(var.last_name)}${count.index + 1}"
-  resource_group_name = homework20024-rg.rg.name
-  location = homework20024-rg.rg.location
-  account_tier = "Standard"
-  account_replication_type = "LRS"
-}
