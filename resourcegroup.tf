90c487fe67a5c97117ec87809487bd472b4a91e8
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-04 15:06:51 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,11 +2,12 @@ resource "azurerm_resource_group" "example" {
  name     = "homework20024-rg"
  location = "Canada East"
 }
-resource "azurerm_storage_account" "example" {
- for_each             = toset(var.homework2024-rg)
- name                 = each.value
- resource_group_name = azurerm_resource_group.homework2024-rg
- location             = azurerm_resource_group.canadaeast
- account_tier         = "Standard"
- account_replication_type = "LRS"
+
+resource "azurerm_storage_account" "storage" {
+  count = 5
+  name = "${lower(var.first_name)}${lower(var.last_name)}${count.index + 1}"
+  resource_group_name = homework20024-rg.rg.name
+  location = homework20024-rg.rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
 }
