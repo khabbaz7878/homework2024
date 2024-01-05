c74f5504027fdf44a1520a742f7d37d111610ca7
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-05 17:24:24 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,21 @@ variable "first_name" {
 }
 
 variable "last_name" {
-  default = "khabbaz" 
+  default = "khabbaz"
 }
 
+variable "subscription_id" {
+  default = "subscription_id"
+}
 
+variable "client_id" {
+  default = "client_id"
+}
+
+variable "client_secret" {
+  default = "client_secret"
+}
+
+variable "tenant_id" {
+  default = "tenant_id"
+}
